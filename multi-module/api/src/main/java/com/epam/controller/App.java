02a3e0667d991a088d64993ad8e29c43d293f27d
Jan <jabajaryn@gmail.com>
2020-10-24 12:01:06 +0300

    changes via demo

--- multi-module/api/src/main/java/com/epam/controller/App.java
+++ multi-module/api/src/main/java/com/epam/controller/App.java
@@ -5,6 +5,6 @@ import com.epam.service.Utils;
 public class App {
     public static void main(String[] args) {
         Utils utils = new Utils();
-        System.out.println("utils.isAllPositiveNumbers(\"23\",\"23\") = " + utils.isAllPositiveNumbers("23", "23"));
+        System.out.println("utils.isAllPositiveNumbers(\"23\",\"23\") = " + utils.isAllPositiveNumbers("-4.4"));
     }
 }
