736ca637348780c6c3ae9a3cdaf5b79785ad8e00
Jan <jabajaryn@gmail.com>
2020-11-20 17:32:52 +0300

    templated true issue resolved

--- giftcertificates/api/src/main/java/com/epam/mjc/api/controller/OrderController.java
+++ giftcertificates/api/src/main/java/com/epam/mjc/api/controller/OrderController.java
@@ -5,15 +5,18 @@ import com.epam.mjc.api.model.dto.OrderDto;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/orders")
 public interface OrderController {
     @GetMapping
     ResponseEntity<CollectionModel<OrderDto>> search(Integer pageNumber, Integer pageSize, Long userId);
 
     @GetMapping("/{id}")
-    ResponseEntity<OrderDto> findById(Long id);
+    ResponseEntity<OrderDto> findById(@PathVariable Long id);
 
     @PostMapping
     ResponseEntity<OrderDto> create(@RequestBody OrderForCreate orderForCreate);
