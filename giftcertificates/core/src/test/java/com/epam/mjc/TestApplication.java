2aaae9fe3daf592cacfcdf6c6e37e4cb92b8aba9
Jan <jabajaryn@gmail.com>
2020-11-24 02:05:55 +0300

    a

--- giftcertificates/core/src/test/java/com/epam/mjc/TestApplication.java
+++ giftcertificates/core/src/test/java/com/epam/mjc/TestApplication.java
@@ -1,14 +1,15 @@
 package com.epam.mjc;
 
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.context.TestConfiguration;
 
-@TestConfiguration
+//@TestConfiguration
+@SpringBootConfiguration
 //@ComponentScan(basePackages = {"com.epam.mjc.api","com.epam.mjc.core.dao","com.epam.mjc.core.config"})
 //@ComponentScan(basePackages = {"com.epam.mjc.api","com.epam.mjc.core.dao","com.epam.mjc.core.config"})
 @EnableAutoConfiguration
 //@SpringBootApplication
-public class TestApplication {
+public class TestApplication extends Application {
 
 //    public static void main(String[] args) {
 //        SpringApplication.run(TestApplication.class);
--- giftcertificates/core/src/test/java/com/epam/mjc/core/dao/TagDaoImplTest.java
+++ giftcertificates/core/src/test/java/com/epam/mjc/core/dao/TagDaoImplTest.java
@@ -1,18 +1,15 @@
 package com.epam.mjc.core.dao;
 
-import com.epam.mjc.core.config.TestConfig;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@ExtendWith(SpringExtension.class)
-//@DataJpaTest
+//@ExtendWith(SpringExtension.class)
+@DataJpaTest
 //@ComponentScan(basePackages = {"com.epam.mjc"})
-@SpringBootTest(classes = TestConfig.class)
+//@SpringBootTest(classes = TestConfig.class)
 //@ContextConfiguration(classes = {TestConfig.class})
 class TagDaoImplTest {
 
