204f5232c2f6757fadf0bf839a98258d960d3595
Jan <jabajaryn@gmail.com>
2020-12-11 19:39:49 +0300

    some changes

--- giftcertificates/api/src/main/java/com/epam/mjc/api/dao/UserRepo.java
+++ giftcertificates/api/src/main/java/com/epam/mjc/api/dao/UserRepo.java
@@ -4,13 +4,9 @@ import com.epam.mjc.api.domain.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepo extends PagingAndSortingRepository<User, Long> {
 
     User findByUsername(String username);
-
-    Optional<User> findByAccessToken(String accessToken);
 
 }
