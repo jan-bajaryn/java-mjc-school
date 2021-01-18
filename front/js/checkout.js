0b196955268c412f99b750074913dff454cc2c16
Jan <jabajaryn@gmail.com>
2021-01-18 14:45:04 +0300

    last scroll position in checkout

--- front/js/checkout.js
+++ front/js/checkout.js
@@ -1,3 +1,15 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    let item = localStorage.getItem('last-scroll-position');
+    if (item) {
+        document.body.scrollTop = Number.parseInt(item);
+    }
+});
+
+
+window.addEventListener('scroll', () => {
+    localStorage.setItem('last-scroll-position', document.body.scrollTop);
+})
+
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
