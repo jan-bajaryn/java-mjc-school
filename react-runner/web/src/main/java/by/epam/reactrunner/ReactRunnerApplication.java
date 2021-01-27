cc0938e540237f5bf47f78d501143dbad85feeda
Jan <jabajaryn@gmail.com>
2021-01-27 21:46:46 +0300

    runner instead of simple react app

---
+++ react-runner/web/src/main/java/by/epam/reactrunner/ReactRunnerApplication.java
@@ -0,0 +1,13 @@
+package by.epam.reactrunner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactRunnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactRunnerApplication.class, args);
+	}
+
+}
---
+++ react-runner/web/src/test/java/by/epam/reactrunner/ReactRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.epam.reactrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
