8fdd8c008da4afa47638fc75f910769a3ed76a8d
Jan <jabajaryn@gmail.com>
2021-02-01 14:42:11 +0300

    screenshots of working application

--- react-runner/web/src/main/java/by/epam/reactrunner/controller/TemplateFile.java
+++ react-runner/web/src/main/java/by/epam/reactrunner/controller/TemplateFile.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TemplateFile {
 
-    @RequestMapping(value = {"/{p:^(?!index.html).*$}", "/*/*/**"})
+    @RequestMapping(value = {"/{p:^(?!index.html).*$}"})
     public String homePage() {
         return "/index.html";
     }
+
+    @RequestMapping(value = {"/{p:^(?!static).*$}/*/**"})
+    public String others() {
+        return "/index.html";
+    }
+
 }
